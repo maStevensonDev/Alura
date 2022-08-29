f2eb846d8278928ef0be858dad1a2ae7abcbb58a
Marco Stevenson <39192921+korexz@users.noreply.github.com>
2022-08-29 16:19:21 -0400

    clase01 completada

---
+++ javascript/holamundo.js
@@ -0,0 +1,13 @@
+console.log(`Trabajando con Listas`);
+
+const Bogota = "Bogota"
+const Lima = "Lima"
+const Santiago = "Santiago"
+const BuenosAires = "Buenos Aires"
+
+
+
+
+const ciudadesDisponibles = ['Bogota','Lima','Santiago','Buenos Aires'];
+
+console.log(ciudadesDisponibles);
