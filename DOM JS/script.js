35625c8af63825bbdf3434b572e93d8771f860c1
Marco Stevenson <39192921+korexz@users.noreply.github.com>
2022-08-30 18:10:24 -0400

    clase03 completada

--- DOM JS/script.js
+++ DOM JS/script.js
@@ -2,11 +2,32 @@ const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento)=> {
   event.preventDefault();
-  const input =  document.querySelector('[data-form-input]');
-  console.log( input.value ); 
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  const list = document.querySelector("[data-list]")
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = "";
+  const content = `<div>
+  <i class="far fa-check-square icon"></i>
+  <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
+  console.log(content);
 }
 
 console.log( btn ); 
 
 btn.addEventListener('click', createTask );
 
+
+
+
+
+
+ 
